fa8dbe654e85c6c0deb2d1e711ce52514279273a
iveal <iveal@IVE-PC>
2024-03-13 13:32:26 -0400

    Add project files.

---
+++ HW 10-2/HW 10-2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace MiniCalculator;
+
+class Program
+{
+    static  ILogger Logger { get; set; }
+    static void Main()
+    {
+        Logger = new Logger();
+        MiniCalculator minicalculator = new MiniCalculator();
+        minicalculator.Calculate();
+    }
+}
+public interface ICalculator
+{
+    void Calculate();
+}
+public interface ILogger
+{
+    void Event(string message);  
+    void Error(string message);
+}
+public class Logger : ILogger
+{
+    public void Event(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine(message);
+    }        
+    public void Error(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+    }
+}
+public class MiniCalculator : Logger, ICalculator
+{
+    ILogger Logger { get; }
+    public void Calculate()
+    {      
+        bool validInput = false;
+        do
+        {
+            try
+            {
+                Logger.Event("Enter the first number for addition:");
+                long a = Convert.ToInt64(Console.ReadLine());
+                Logger.Event("Enter the second number for addition:");
+                long b = Convert.ToInt64(Console.ReadLine());
+                var result = a + b;
+                Logger.Event("Here is your result: " + result);
+                validInput = true;
+            }
+            catch (FormatException)
+            {
+                Logger.Error("What you entered is NOT a number!");
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        } while (!validInput);
+    }
+}
---
+++ Module10/HW 10-1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace MiniCalculator;
+
+class Program
+{
+    static void Main()
+    {
+        MiniCalculator minicalculator = new MiniCalculator();
+        minicalculator.Calculate();
+    }
+}
+public interface ICalculator
+{
+    void Calculate();
+}
+public class MiniCalculator : ICalculator
+{
+    public void Calculate()
+    {
+        bool validInput = false;
+        do
+        {
+            try
+            {
+                Console.WriteLine("Hello \n Enter the first number for addition:");
+                long a = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine("Now enter the second number for addition:");
+                long b = Convert.ToInt64(Console.ReadLine());
+                var result = a + b;
+                Console.WriteLine("Here is your result: " + result);
+                validInput = true;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("What you entered is NOT a number!");
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        } while (!validInput);
+    }
+}
